a6d59ea6ace9005836dd1efb01fe1998797e5787
Razieh <razi.benvidi98@gmail.com>
2024-06-10 18:47:04 -0400

    Assignment Done

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,42 @@ app.use(express.json());
 app.post(`/post`, async (req, res) => {
   //create a new post and associate it with an author
   const { title, content, authorEmail } = req.body;
-  const result = await prisma;
-  res.json(result);
+  try {
+    const author = await prisma.user.findUnique({
+      where: { email: authorEmail },
+    });
+    if (!author) {
+      return res.status(404).json({ error: "Author not found" });
+    }
+    const result = await prisma.post.create({
+      data: {
+        title,
+        content,
+        author: {
+          connect: { email: authorEmail },
+        },
+      },
+    });
+    res.json(result);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the post" });
+  }
 });
 
 app.put("/post/:id/views", async (req, res) => {
   const { id } = req.params;
   //update the view count field for a specific post
   try {
-    const post = await prisma;
+    const post = await prisma.post.update({
+      where: { id: Number(id) },
+      data: {
+        viewCount: {
+          increment: 1,
+        },
+      },
+    });
 
     res.json(post);
   } catch (error) {
@@ -30,7 +57,15 @@ app.put("/publish/:id", async (req, res) => {
 
   try {
     // toggle the `published` field on the specified post
-    const updatedPost = await prisma;
+    const post = await prisma.post.findUnique({
+      where: { id: Number(id) },
+    });
+    const updatedPost = await prisma.post.update({
+      where: { id: Number(id) },
+      data: {
+        published: !post?.published,
+      },
+    });
 
     res.json(updatedPost);
   } catch (error) {
@@ -41,28 +76,52 @@ app.put("/publish/:id", async (req, res) => {
 app.delete(`/post/:id`, async (req, res) => {
   //delete the post
   const { id } = req.params;
-  const post = await prisma;
-  res.json(post);
+  try {
+    const post = await prisma.post.delete({
+      where: { id: Number(id) },
+    });
+    res.json(post);
+  } catch (error) {
+    res.json({ error: `Post with ID ${id} does not exist in the database` });
+  }
 });
 
 app.get("/users", async (req, res) => {
   //return all the users
-  const users = await prisma;
+  const users = await prisma.user.findMany();
   res.json(users);
 });
 
 app.get("/user/:id/drafts", async (req, res) => {
   const { id } = req.params;
   //return all posts where the published field equals false
-  const drafts = await prisma;
+  const drafts = await prisma.post.findMany({
+    where: {
+      author: {
+        id: Number(id),
+      },
+      published: false,
+    },
+  });
   res.json(drafts);
 });
 
 app.get(`/post/:id`, async (req, res) => {
   const { id }: { id?: string } = req.params;
   //return the post
-  const post = await prisma;
-  res.json(post);
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id: Number(id) },
+    });
+    if (!post) {
+      return res
+        .status(404)
+        .json({ error: `Post with ID ${id} does not exist in the database` });
+    }
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
 });
 
 app.get("/feed", async (req, res) => {
@@ -74,11 +133,43 @@ app.get("/feed", async (req, res) => {
   // 5. order the posts by the field `updated_at` descending or ascending basesd on the parameter `orderBy`
   // 6. if the `searchString` parameter is not an empty, use the string to filter posts not matching the post titles or post content
 
-  const posts = await prisma.post.findMany();
-
+  const posts = await prisma.post.findMany({
+    where: {
+      AND: [
+        {
+          published: true,
+        },
+        {
+          OR: [
+            {
+              title: {
+                contains: String(searchString),
+              },
+            },
+            {
+              content: {
+                contains: String(searchString),
+              },
+            },
+          ],
+        },
+      ],
+    },
+    include: {
+      author: true,
+    },
+    skip: Number(skip),
+    take: Number(take),
+    orderBy: {
+      updatedAt: orderBy === "desc" ? "desc" : "asc",
+    },
+  });
   res.json(posts);
 });
 
+app.get("/", async (req, res) => {
+  res.json({ messae: "Hello World" });
+});
 const server = app.listen(3000, () =>
   console.log(`
 🚀 Server ready at: http://localhost:3000`)
